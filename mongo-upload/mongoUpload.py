590b753137f4bf865fc75effaecbb96538d3058c
OnlyPascal <chuyengianhorang@gmail.com>
2024-12-17 11:26:18 +0700

    temp save

--- mongo-upload/mongoUpload.py
+++ mongo-upload/mongoUpload.py
@@ -5,29 +5,50 @@ import pandas as pd
 from dotenv import load_dotenv
 
 # load env
+print('> Loading env ...')
 load_dotenv()
-
-
 def get_env(envName, default):
     return os.environ.get(envName) if os.environ.get(envName) else default
-
-
 MONGO_URL = get_env('MONGO_URL', 'None')
+
 # load csv
-print('=== read csv ===')
+print('> Getting Data...')
 dataDir = 'data'
-csvFiles = [f'{dataDir}/{file}' for file in os.listdir(dataDir)]
+energyTypes = ['Electricity', 'Gas']
 
-# read
-csvFile = csvFiles[0]
-df = pd.read_csv(csvFile)
-# print(df.head(10))
-
-# connect db
-print('=== connect db ===')
+# init db
 client = pymongo.MongoClient(MONGO_URL)
-db = client.get_database('db_test1')
-coll = db['coll_test1']
+db = client.get_database('asm2-test1')
+# coll = db['']
 
-coll.drop()
-coll.insert_many(df.to_dict('records'))
+for energyType in energyTypes:
+    print(f'>> {energyType}')
+    # scan files
+    dataSubdir = f'{dataDir}/{energyType}'
+    csvFiles = [f'{dataSubdir}/{file}' for file in os.listdir(dataSubdir)]
+    energyDf = pd.DataFrame()
+    
+    coll = db[energyType.lower()]
+    coll.drop()
+
+    print('- Loading csv')
+    for csvFile in csvFiles:
+        metadata = csvFile.split('/')[-1].split('.')[0].split('_')
+        
+        # extract company, year
+        company = metadata[0]
+        year = metadata[-1]
+        
+        # read csv + add collumn 
+        df = pd.read_csv(csvFile)
+        df['company'] = company
+        df['year'] = year
+        
+        # gathering
+        energyDf = pd.concat([energyDf, df])
+    
+        # 
+        print(f'- Uploading {csvFile}')
+        coll.insert_many(df.to_dict('records'))
+    
+print('> Done!')
